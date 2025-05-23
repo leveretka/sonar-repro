6bf06a48de5f27a6b1846f769140d1ba54d89a69
satyarth <satyarth.sampath@phonepe.com>
2025-05-23 22:34:39 +0530

    adds the sonar plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,13 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.jetbrains.kotlin.kapt) apply false
     alias(libs.plugins.hilt.android) apply false
+    alias(libs.plugins.sonar) apply true
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "contact-sync")
+        property("sonar.host.url", "https://sonarqube.acme.com/")
+        property("sonar.token", System.getenv("SONAR_TOKEN"))
+    }
 }
